cab752b88f9016c9b624c731424105cf7ff3c4a1
Rafael Macedo <rafael.macedo@EFICAZ-52R.eficaz.local>
2021-02-22 19:11:50 -0300

    Criado as models e controllers do UserTest e Endereço

--- database/seeders/UsersTestsSeeder.php
+++ database/seeders/UsersTestsSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+
+use Eficaz\Affiliates\Models\UserTest;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UsersTestsSeeder extends Seeder
 {
@@ -13,6 +16,27 @@ class UsersTestsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = [
+            [
+                'seu_e-mail' => trans('front@eficazconsultoria.net.br'),
+                'nome' => trans('Rafael Eficaz'),
+                'e-mail' => Str::slug('emailrafael@eficazconsultoria.net.br'),
+                'telefone' => trans('(14) 12345-1234'),
+            ],
+            [
+                'seu_e-mail' => trans('front@eficazconsultoria.net.br'),
+                'nome' => trans('Maria Eficaz'),
+                'e-mail' => Str::slug('emailmaria@eficazconsultoria.net.br'),
+                'telefone' => trans('(14) 54321-4321'),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            if (UserTest::where('nome', $user['nome'])->exists()) {
+                continue;
+            }
+
+            UserTest::create($accountType);
+        }
     }
 }
